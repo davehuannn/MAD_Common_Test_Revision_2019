06987dbb8c72d49cb34ab0fb54e04dd748ef2e65
Dave Huan <davehuan123@gmail.com>
2019-12-03 14:21:21 +0800

    Initial Commit

---
+++ CommonTestRevision2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CommonTestRevision2019
+//
+//  Created by MAD2_P03 on 3/12/19.
+//  Copyright © 2019 Dave Huan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
